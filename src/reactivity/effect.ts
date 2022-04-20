7a02e797473a77bbe35e742440399f0ab5ee75e5
605572714 <605572714@qq.com>
2022-04-20 22:38:04 +0800

    readonly stop onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,17 +1,38 @@
+import { extend } from "../shared";
+
 class ReactiveEffect {
-  private _fn: any
-  constructor(fn) {
+  private _fn: any;
+  deps = [];
+  active = true
+  public scheduler: Function | undefined;
+  onStop?: () => void;
+  constructor(fn, scheduler?: Function) {
     this._fn = fn
+    this.scheduler = scheduler
   }
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
+
+  stop() {
+    if (this.active) {
+      cleanupEffect(this)
+      if (this.onStop) this.onStop();
+      this.active = false
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 
 const targetMap = new Map()
-export function track(target: any, key: string | symbol) {
+export function track(target, key) {
   // target -> key -> dep
   let depsMap = targetMap.get(target)
   if (!depsMap) {
@@ -23,20 +44,35 @@ export function track(target: any, key: string | symbol) {
     dep = new Set()
     depsMap.set(key, dep)
   }
+  if (!activeEffect) return
   dep.add(activeEffect)
-  // const dep = new Set()
+  activeEffect.deps.push(dep)
 }
-export function trigger(target: any, key: string | symbol) {
+export function trigger(target, key) {
   let depsMap = targetMap.get(target)
   let dep = depsMap.get(key)
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler) effect.scheduler()
+    else effect.run()
   }
 }
 
 
 let activeEffect;
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+  // fn
+  const _effect = new ReactiveEffect(fn, options.scheduler)
+  // options
+  // Object.assign(_effect, options)
+  // extend
+  extend(_effect, options)
+
   _effect.run()
+
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
+}
+export function stop(runner) {
+  runner.effect.stop()
 }
