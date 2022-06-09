fef482d3cd36d55cef9f31975ba699bb185fbdf3
Theseus <605572714@qq.com>
2022-06-09 15:59:51 +0800

    基础slots
    只支持传入一个p标签

---
+++ example/componentSlot/Foo.js
@@ -0,0 +1,12 @@
+import {
+  h
+} from '../../lib/guide-mini-vue.esm.js';
+export const Foo = {
+  setup () {
+    return {}
+  },
+  render () {
+    const foo = h("p", {}, "foo")
+    return h("div", {}, [foo, this.$slots])
+  }
+}
