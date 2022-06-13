a2a43b47e47c0271133d4a6af1612b7eb171eb90
Theseus <605572714@qq.com>
2022-06-13 17:48:45 +0800

    初始化apiinject

---
+++ example/apiInject/App.js
@@ -0,0 +1,16 @@
+import {
+  h, getCurrentInstance
+} from '../../lib/guide-mini-vue.esm.js';
+import { Foo } from './Foo.js';
+window.self = null
+export const App = {
+  name: "App",
+  render () {
+    return h("div", {}, [h("p", {}, "currentInstance dome"), h(Foo)])
+  },
+
+  setup () {
+    const instance = getCurrentInstance()
+    console.log("APP:", instance);
+  }
+}
