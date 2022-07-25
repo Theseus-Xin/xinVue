1ee3c81716f897b783446a1bb826b1c12dc03234
Theseus <605572714@qq.com>
2022-07-25 18:07:02 +0800

    diff 最长递增子序列

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -54,12 +54,49 @@ import { ref, h } from "../../lib/guide-mini-vue.esm.js"
 //   h("div", { key: "B" }, "B")
 // ]
 
-// // 5.中间不同
+// // // 5.中间不同
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C", id: "c-prev" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ]
+
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "C", id: "c-next" }, "C"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ]
+// // // 5.中间移动
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ]
+
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ]
+// 6.最长递增子序列
 const prevChildren = [
   h("div", { key: "A" }, "A"),
   h("div", { key: "B" }, "B"),
-  h("div", { key: "C", id: "c-prev" }, "C"),
+  h("div", { key: "C" }, "C"),
   h("div", { key: "D" }, "D"),
+  h("div", { key: "E" }, "E"),
   h("div", { key: "F" }, "F"),
   h("div", { key: "G" }, "G"),
 ]
@@ -68,11 +105,11 @@ const nextChildren = [
   h("div", { key: "A" }, "A"),
   h("div", { key: "B" }, "B"),
   h("div", { key: "E" }, "E"),
-  h("div", { key: "C", id: "c-next" }, "C"),
+  h("div", { key: "C" }, "C"),
+  h("div", { key: "D" }, "D"),
   h("div", { key: "F" }, "F"),
   h("div", { key: "G" }, "G"),
 ]
-
 export default {
   name: "ArrayToText",
   setup () {
