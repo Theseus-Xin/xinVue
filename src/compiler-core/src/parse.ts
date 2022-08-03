a9c7e124b6ca6fb34da8c26ada6924843784f5c6
Theseus <605572714@qq.com>
2022-08-03 15:51:42 +0800

    elment

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,6 +1,9 @@
 import { NodeTypes } from "./ast"
 
-
+const enum TagType {
+  Start,
+  End
+}
 
 export function baseParse(content: string) {
   const context = createParserContext(content)
@@ -11,8 +14,13 @@ function parseChildren(context) {
   const nodes = [] as any[]
 
   let node
-  if (context.source.startsWith("{{")) {
+  const s = context.source
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context)
+  } else if (s[0] == "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
 
   nodes.push(node)
@@ -66,3 +74,30 @@ function advanceBy(context, length) {
   context.source = context.source.slice(length)
 
 }
+
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start)
+  parseTag(context, TagType.End)
+  console.log("element", context.source);
+
+  return element
+}
+function parseTag(context: any, type: TagType) {
+  // implement
+  // 解析tag
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  // console.log("match", match);
+
+  const tag = match[1]
+
+  // 删除处理完成的代码
+
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+  if (type === TagType.End) return
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
+}
+
